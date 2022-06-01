471705e07c0e19e37909ecf3ac52e382ba23a1bb
EduBarrros <74619318+EduBarrros@users.noreply.github.com>
2022-05-31 22:05:10 -0300

    feat: Password encryptation during user registry

--- routes/login_criar.js
+++ routes/login_criar.js
@@ -1,13 +1,33 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+const fs = require('fs')
 const router = express.Router()
 
+const dadosLocais = JSON.parse(fs.readFileSync("dados.json"))
 
 router.post('/login', (req, res) => {
     res.status(200).send("OK Login");
 })
 
 router.post('/criar', (req, res) => {
-    res.status(200).send("OK Criar");
+    const { nome, email, senha } = req.body
+    if(!email || !senha){
+        res.status(422).send('Você deve definir um email e uma senha.')
+    }else if(dadosLocais.find((usuario) => usuario.nome === nome || usuario.email === email)){
+        res.status(401).send('Nome ou email de usuário já está em uso.')
+    }else{
+        var dadosUsuario = {
+            nome: nome,
+            email: email,
+            dados: {}
+        }
+        const salt = bcrypt.genSaltSync(10)
+        dadosUsuario.hash = bcrypt.hashSync(senha, salt)
+        dadosLocais.push(dadosUsuario)
+        const dadosConvertidos = JSON.stringify(dadosLocais, null, 2)
+        fs.writeFileSync("dados.json", dadosConvertidos)
+        res.status(200).send("Usuário cadastrado com sucesso!");
+    }
 })
 
 module.exports = router
