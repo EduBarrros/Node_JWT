e809d06bc0daf90cc415c5f232b81531df0c1607
EduBarrros <74619318+EduBarrros@users.noreply.github.com>
2022-06-01 23:19:55 -0300

    feat: finish login verification

--- routes/login_criar.js
+++ routes/login_criar.js
@@ -6,16 +6,36 @@ const router = express.Router()
 const dadosLocais = JSON.parse(fs.readFileSync("dados.json"))
 
 router.post('/login', (req, res) => {
-    res.status(200).send("OK Login");
+    const { email, senha } = req.body
+
+    if (!email || !senha) {
+        res.status(422).send('Você deve definir um email e uma senha.')
+    }
+
+    const usuario = dadosLocais.find((user) => user.email === email)
+
+    if (!usuario) {
+        res.status(401).send('Email ou senha inválidos.')
+    }
+
+    if (!bcrypt.compareSync(senha, usuario.hash)) {
+        res.status(401).send('Email ou senha inválidos.')
+    }
+
+    res.status(200).send({
+        nome: usuario.nome,
+        email: usuario.email,
+        dados: usuario.dados
+    })
 })
 
 router.post('/criar', (req, res) => {
     const { nome, email, senha } = req.body
-    if(!email || !senha){
+    if (!email || !senha) {
         res.status(422).send('Você deve definir um email e uma senha.')
-    }else if(dadosLocais.find((usuario) => usuario.nome === nome || usuario.email === email)){
+    } else if (dadosLocais.find((usuario) => usuario.nome === nome || usuario.email === email)) {
         res.status(401).send('Nome ou email de usuário já está em uso.')
-    }else{
+    } else {
         var dadosUsuario = {
             nome: nome,
             email: email,
